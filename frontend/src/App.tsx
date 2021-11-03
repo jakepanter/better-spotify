fab335f52b71351e23b78b8a3b6adecb1d266f69
Nomi-1 <schoppanaomi94@gmail.com>
2021-11-03 09:11:58 +0100

    49 grid mixin - setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,21 @@ function App() {
         </ul>
 
         <p>I just need to check the basic style, please feel free to delete this later</p>
+        <div className="test">
+          <div className="box">1</div>
+          <div className="box">2</div>
+          <div className="box">3</div>
+          <div className="box">4</div>
+          <div className="box">5</div>
+          <div className="box">6</div>
+          <div className="box">7</div>
+          <div className="box">8</div>
+          <div className="box">9</div>
+          <div className="box">10</div>
+          <div className="box">11</div>
+          <div className="box">12</div>
 
+        </div>
         <Switch>
           <Route path="/">
             <h1>Works</h1>
