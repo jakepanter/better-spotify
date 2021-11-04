880a4debc31efdb0dd6930c78878fd7f2110cbf6
Lennard Grimm <hi@grimmle.com>
2021-11-04 15:55:23 +0100

    add components and their routes to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import Album from "./components/Album/Album";
+import Albums from "./components/Albums/Albums";
 import Searchbar from "./components/Searchbar/Searchbar";
 
 function authorize() {
@@ -16,21 +18,41 @@ function App() {
   return (
     <Router>
       <div>
-        <Searchbar/>
+        <Searchbar />
         <ul>
           <li>
-            <Link className="button" to="/">Home</Link>
+            <Link className="button" to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <button className="button" onClick={authorize}>Authorize</button>
+            <Link className="button" to="/collections/albums">
+              Saved Albums
+            </Link>
+          </li>
+          <li>
+            <button className="button" onClick={authorize}>
+              Authorize
+            </button>
           </li>
         </ul>
 
-        <p>I just need to check the basic style, please feel free to delete this later</p>
+        <p>
+          I just need to check the basic style, please feel free to delete this
+          later
+        </p>
 
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <h1>Works</h1>
+          </Route>
+          <Route path="/album/:id">
+            <h1>Album</h1>
+            <Album />
+          </Route>
+          <Route path="/collections/albums">
+            <h1>Albums</h1>
+            <Albums />
           </Route>
         </Switch>
       </div>
