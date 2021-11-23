7065cc3d22b4991daad7be0b31e972a6485e14e4
Stefan Böhnert <boehnert.stefan@gmail.com>
2021-11-23 13:41:02 +0100

    Update frontend/src/components/TrackListItem/TrackListItem.tsx
    Co-authored-by: Phillip <phillipkiele@gmail.com>

--- frontend/src/components/TrackListItem/TrackListItem.tsx
+++ frontend/src/components/TrackListItem/TrackListItem.tsx
@@ -12,7 +12,6 @@ function TrackListItem(props: Props) {
   //const type = props.type
 
   const [track, setTrack] = useState<TrackObjectFull>();
-  console.log(id);
   useEffect(() => {
     async function fetchData() {
       const data: TrackObjectFull = await fetch(
