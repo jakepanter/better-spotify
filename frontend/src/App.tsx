de8e83318715eac3952f3d171d1d4de444ed474b
Nomi-1 <schoppanaomi94@gmail.com>
2021-11-25 21:34:08 +0100

    #87 - and #88 add style and structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import {NavLink} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.scss";
 import Album from "./components/Album/Album";
 import Albums from "./components/Albums/Albums";
@@ -9,84 +10,115 @@ import Searchbar from "./components/Searchbar/Searchbar";
 import SavedTracks from "./components/SavedTracks/SavedTracks"
 import Player from "./components/Player/Player";
 
+
 function authorize() {
-  fetch(`http://localhost:5000/api/spotify/get-auth-url`)
-    .then((res) => res.text())
-    .then((url) => {
-      console.log(url);
-      window.location.href = url;
-    });
+    fetch(`http://localhost:5000/api/spotify/get-auth-url`)
+        .then((res) => res.text())
+        .then((url) => {
+            console.log(url);
+            window.location.href = url;
+        });
 }
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Searchbar/>
-        <Player/>
-        <ul>
-          <li>
-            <Link className="button" to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="button" to="/collections/albums">
-              Saved Albums
-            </Link>
-          </li>
-          <li>
-            <Link className="button" to="/playlists">
-              My Playlists
-            </Link>
-          </li>
-          <li>
-            <button className="button" onClick={authorize}>
-              Authorize
-            </button>
-          </li>
-          <li>
-            <Link className="button" to="/me/tracks">Saved Tracks</Link>
-          </li>
-        </ul>
-        
-        <Switch>
-          <Route exact path="/">
-            <h1>Home</h1>
-          </Route>
-          <Route path="/playlists">
-            <Playlists />
-          </Route>
-          <Route path="/playlist/:id">
-            <Playlist />
-          </Route>
-          <Route path="/album/:id">
-            <h1>Album</h1>
-            <Album />
-          </Route>
-          <Route path="/collections/albums">
-            <h1>Albums</h1>
-            <Albums />
-          </Route>
-          <Route path="/me/tracks">
-            <h1>Saved Tracks</h1>
-            <SavedTracks/>
-          </Route>
-        </Switch>
-      </div>
-      <div className="columns-test">
-          <div className="column">Test</div>
-          <div className="column">Test</div>
-      </div>
-      <div className="grid-test">
-        <div className="grid-box">1</div>
-        <div className="grid-box">2</div>
-        <div className="grid-box">3</div>
-        <div className="grid-box">4</div>
+    return (
+        <Router>
+            <div className="structure">
+                <div className="structure--left-panel">
+                    <ul className="left-side-panel">
+                        <li>
+                            <NavLink exact activeClassName="active" className="button" to="/">
+                                <span className="material-icons">home</span>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="/me/tracks">
+                                <span className="material-icons">favorite</span>
+                                Favorites
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="/playlists">
+                                <span className="material-icons">queue_music</span>
+                                Playlists
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="/collections/albums">
+                                <span className="material-icons">library_music</span>
+                                Albums
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="">
+                                    <span className="material-icons">grid_view</span>
+                                    Library
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="">
+                                    <span className="material-icons">manage_search</span>
+                                    Discover
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" className="button" to="">
+                                    <span className="material-icons">queue</span>
+                                    Queue
+                            </NavLink>
+                        </li>
+                    </ul>
 
-      </div>
-    </Router>
-  );
+                </div>
+                <div className="structure--main">
+                    <Searchbar/>
+                    <Player/>
+                    <ul>
+                        <li>
+                            <button className="button" onClick={authorize}>
+                                Authorize
+                            </button>
+                        </li>
+                    </ul>
+                    <Switch>
+                        <Route exact path="/">
+                            <h1>Home</h1>
+                        </Route>
+                        <Route path="/playlists">
+                            <Playlists/>
+                        </Route>
+                        <Route path="/playlist/:id">
+                            <Playlist/>
+                        </Route>
+                        <Route path="/album/:id">
+                            <h1>Album</h1>
+                            <Album/>
+                        </Route>
+                        <Route path="/collections/albums">
+                            <h1>Albums</h1>
+                            <Albums/>
+                        </Route>
+                        <Route path="/me/tracks">
+                            <h1>Saved Tracks</h1>
+                            <SavedTracks/>
+                        </Route>
+                    </Switch>
+                    <div className="columns-test">
+                        <div className="column">Test</div>
+                        <div className="column">Test</div>
+                    </div>
+                    <div className="grid-test">
+                        <div className="grid-box">1</div>
+                        <div className="grid-box">2</div>
+                        <div className="grid-box">3</div>
+                        <div className="grid-box">4</div>
+
+                    </div>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
