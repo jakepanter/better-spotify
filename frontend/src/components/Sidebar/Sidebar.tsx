f0b951dc9b32940247a2098850e44a38892c6fa6
Nomi-1 <schoppanaomi94@gmail.com>
2021-11-27 09:59:48 +0100

    Fixed mobile menu and optimized code

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,53 +1,72 @@
-import React from "react";
-import './Sidebar.scss';
+import React, {useState} from "react";
 import {NavLink} from "react-router-dom";
+import './Sidebar.scss';
 
 const Sidebar = () => {
+    {/* Mobile toggle setup */}
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+    }
+    const closeMenu = () => {
+        setNavbarOpen(false)
+    }
     return (
-        <ul className="left-side-panel">
-            <li>
-                <NavLink exact activeClassName="active" className="button" to="/">
-                    <span className="material-icons left-side-panel--icon">home</span>
-                    <span className="left-side-panel--text">Home</span>
-                </NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" className="button" to="/me/tracks">
-                    <span className="material-icons left-side-panel--icon">favorite</span>
-                    <span className="left-side-panel--text">Favorites</span>
-                </NavLink>
-            </li>
-            <li className="hiddenMenu">
-                <NavLink activeClassName="active" className="button" to="/playlists">
-                    <span className="material-icons left-side-panel--icon">queue_music</span>
-                    <span className="left-side-panel--text">Playlists</span>
-                </NavLink>
-            </li>
-            <li className="hiddenMenu">
-                <NavLink activeClassName="active" className="button" to="/collections/albums">
-                    <span className="material-icons left-side-panel--icon">library_music</span>
-                    <span className="left-side-panel--text">Albums</span>
-                </NavLink>
-            </li>
-            <li className="hiddenMenu">
-                <NavLink exact activeClassName="active" className="button" to="/">
-                    <span className="material-icons left-side-panel--icon">grid_view</span>
-                    <span className="left-side-panel--text">Library</span>
-                </NavLink>
-            </li>
-            <li className="hiddenMenu">
-                <NavLink exact activeClassName="active" className="button" to="/">
-                    <span className="material-icons left-side-panel--icon">manage_search</span>
-                    <span className="left-side-panel--text">Discover</span>
-                </NavLink>
-            </li>
-            <li className="hiddenMenu">
-                <NavLink exact activeClassName="active" className="button" to="/">
-                    <span className="material-icons left-side-panel--icon">queue</span>
-                    <span className="left-side-panel--text">Queue</span>
-                </NavLink>
-            </li>
-        </ul>
+        <div className={"left-side-panel"}>
+            {/* Only visible in Mobile viewport */}
+            <button className="mobileToggle" onClick={handleToggle}>{navbarOpen ?
+                <p className="material-icons left-side-panel--icon">circle</p> :
+                <p className="material-icons left-side-panel--icon">radio_button_unchecked</p>}
+            </button>
+            {/* Sidebar menu */}
+            <ul className={`left-side-panel--list ${navbarOpen ? " showMenu" : ""}`}>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/" onClick={() => closeMenu()} exact>
+                        <span className="material-icons left-side-panel--icon">home</span>
+                        <span className="left-side-panel--text">Home</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/me/tracks" onClick={() => closeMenu()}
+                             exact>
+                        <span className="material-icons left-side-panel--icon">favorite</span>
+                        <span className="left-side-panel--text">Favorites</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/playlists" onClick={() => closeMenu()}
+                             exact>
+                        <span className="material-icons left-side-panel--icon">queue_music</span>
+                        <span className="left-side-panel--text">Playlists</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/collections/albums"
+                             onClick={() => closeMenu()} exact>
+                        <span className="material-icons left-side-panel--icon">library_music</span>
+                        <span className="left-side-panel--text">Albums</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/" onClick={() => closeMenu()} exact>
+                        <span className="material-icons left-side-panel--icon">grid_view</span>
+                        <span className="left-side-panel--text">Library</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/" onClick={() => closeMenu()} exact>
+                        <span className="material-icons left-side-panel--icon">manage_search</span>
+                        <span className="left-side-panel--text">Discover</span>
+                    </NavLink>
+                </li>
+                <li className="column">
+                    <NavLink activeClassName="active" className="button" to="/" onClick={() => closeMenu()} exact>
+                        <span className="material-icons left-side-panel--icon">queue</span>
+                        <span className="left-side-panel--text">Queue</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
     )
 }
 
