1e0fba72f505d8a336103238038e9144c3194488
Jakob Panten <jakob@panten-sh.de>
2021-12-09 16:52:57 +0100

    add grid to topbar and icon buttons

--- frontend/src/components/Topbar/Topbar.tsx
+++ frontend/src/components/Topbar/Topbar.tsx
@@ -5,7 +5,18 @@ import Searchbar from "../Searchbar/Searchbar";
 const Topbar = () => {
   return (
       <div className={'top-bar'}>
-        <Searchbar />
+          <div className={'top-bar-item'}>
+              <Searchbar />
+          </div>
+          <div className={'top-bar-item volume-slider'}>Lautstärke</div>
+          <div className={'top-bar-item settings'}>
+              <button>
+                  <span className={'material-icons'}>notifications</span>
+              </button>
+              <button>
+                  <span className={'material-icons'}>account_circle</span>
+              </button>
+          </div>
       </div>
   );
 }
