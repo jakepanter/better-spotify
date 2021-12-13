aae672d50953fd64309f88f7128c3e71c503d5cf
Lennard Grimm <hi@grimmle.com>
2021-12-13 17:52:33 +0100

    only show log in button when not authorized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,70 +1,79 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Album from "./components/Album/Album";
 import Albums from "./components/Albums/Albums";
 import Playlist from "./components/Playlist/Playlist";
 import Playlists from "./components/Playlists/Playlists";
 import Searchbar from "./components/Searchbar/Searchbar";
-import SavedTracks from "./components/SavedTracks/SavedTracks"
+import SavedTracks from "./components/SavedTracks/SavedTracks";
 import Player from "./components/Player/Player";
-import {API_URL} from "./utils/constants";
+import { API_URL } from "./utils/constants";
 import Sidebar from "./components/Sidebar/Sidebar";
 
-
 function authorize() {
-    fetch(`${API_URL}api/spotify/get-auth-url`)
-        .then((res) => res.text())
-        .then((url) => {
-            console.log(url);
-            window.location.href = url;
-        });
+  fetch(`${API_URL}api/spotify/get-auth-url`)
+    .then((res) => res.text())
+    .then((url) => {
+      console.log(url);
+      window.location.href = url;
+    });
 }
 
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  useEffect(() => {
+    async function getAccessToken() {
+      const res = await fetch(
+        `http://localhost:5000/api/spotify/access-token`
+      ).then((res) => res.json());
+      setIsAuthorized(res !== undefined);
+    }
+    getAccessToken();
+  }, []);
+
+  if (!isAuthorized)
     return (
-        <Router>
-            <div className="structure">
-                <div className="structure--left-panel">
-                    <Sidebar/>
-                </div>
-                <div className="structure--main">
-                    <Searchbar/>
-                    <Player/>
-                    <ul>
-                        <li>
-                            <button className="button" onClick={authorize}>
-                                Authorize
-                            </button>
-                        </li>
-                    </ul>
-                    <Switch>
-                        <Route exact path="/">
-                            <h1>Home</h1>
-                        </Route>
-                        <Route path="/playlists">
-                            <Playlists/>
-                        </Route>
-                        <Route path="/playlist/:id">
-                            <Playlist/>
-                        </Route>
-                        <Route path="/album/:id">
-                            <h1>Album</h1>
-                            <Album/>
-                        </Route>
-                        <Route path="/collections/albums">
-                            <h1>Albums</h1>
-                            <Albums/>
-                        </Route>
-                        <Route path="/me/tracks">
-                            <h1>Saved Tracks</h1>
-                            <SavedTracks/>
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
-        </Router>
+      <button className="button" onClick={authorize}>
+        Log in with Spotify
+      </button>
     );
+  return (
+    <Router>
+      <div className="structure">
+        <div className="structure--left-panel">
+          <Sidebar />
+        </div>
+        <div className="structure--main">
+          <Searchbar />
+          <Player />
+          <Switch>
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+            <Route path="/playlists">
+              <Playlists />
+            </Route>
+            <Route path="/playlist/:id">
+              <Playlist />
+            </Route>
+            <Route path="/album/:id">
+              <h1>Album</h1>
+              <Album />
+            </Route>
+            <Route path="/collections/albums">
+              <h1>Albums</h1>
+              <Albums />
+            </Route>
+            <Route path="/me/tracks">
+              <h1>Saved Tracks</h1>
+              <SavedTracks />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
