fb00902926c9031a5b3979e5f1b250afe67990e2
Lennard Grimm <hi@grimmle.com>
2021-12-15 17:14:46 +0100

    add cover placeholder everywhere images are used

---
+++ frontend/src/components/CoverPlaceholder/CoverPlaceholder.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CoverPlaceholder.scss";
+
+function CoverPlaceholder() {
+  return <div className="CoverPlaceholder cover"></div>;
+}
+
+export default CoverPlaceholder;
--- frontend/src/components/TrackListItem/TrackListItem.tsx
+++ frontend/src/components/TrackListItem/TrackListItem.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import {AlbumObjectSimplified, ArtistObjectSimplified, TrackObjectFull, TrackObjectSimplified} from "spotify-types";
-import {formatTimeDiff, formatTimestamp} from "../../utils/functions";
+import {
+  AlbumObjectSimplified,
+  ArtistObjectSimplified,
+  TrackObjectFull,
+  TrackObjectSimplified,
+} from "spotify-types";
+import { formatTimeDiff, formatTimestamp } from "../../utils/functions";
 import Checkbox from "../Checkbox/Checkbox";
+import CoverPlaceholder from "../CoverPlaceholder/CoverPlaceholder";
 //import SpotifyWebPlayer from "react-spotify-web-playback";
 
 type Props = {
@@ -18,28 +24,50 @@ function TrackListItem(props: Props) {
   const track = props;
 
   return (
-    <div className={'TableRow'}>
-      {track.album !== undefined ?
-        <div className={'TableCell TableCellArtwork'}>
-          <img style={{width: "40px", height: "40px"}} src={track.album.images[2].url} alt=""/>
-        </div>
-        :
-        <div className={'TableCell TableCellArtwork'} style={{width: "40px", height: "40px"}}/>
-      }
-      <div className={'TableCell TableCellTitleArtist'}>
-        <span className={'TableCellTitle'}>{track.name}</span>
-        <span className={'TableCellArtist'}>{track.artists.map((artist) => artist.name).join(", ")}</span>
+    <div className={"TableRow"}>
+      <div className={"TableCell TableCellArtwork"}>
+        {track.album !== undefined ? (
+          <img
+            style={{ width: "40px", height: "40px" }}
+            src={track.album.images[2].url}
+            alt=""
+          />
+        ) : (
+          <CoverPlaceholder />
+        )}
       </div>
-      {track.album !== undefined ?
-        <div className={'TableCell TableCellAlbum'}>{track.album.name}</div>
-        : <></>}
-      {track.added_at !== undefined ? <div className={'TableCell TableCellAddedAt'}>
-        {formatTimeDiff(new Date(track.added_at).getTime(), Date.now())}
-      </div> : <></>}
-      <div className={'TableCell TableCellDuration'}>{formatTimestamp(track.duration_ms)}</div>
-      {track.liked !== undefined ? <div className={'TableCell TableCellLiked'}>
-        <Checkbox checked={track.liked} iconCodeChecked={'favorite'} iconCodeUnchecked={'favorite_border'}/>
-      </div> : <></>}
+      <div className={"TableCell TableCellTitleArtist"}>
+        <span className={"TableCellTitle"}>{track.name}</span>
+        <span className={"TableCellArtist"}>
+          {track.artists.map((artist) => artist.name).join(", ")}
+        </span>
+      </div>
+      {track.album !== undefined ? (
+        <div className={"TableCell TableCellAlbum"}>{track.album.name}</div>
+      ) : (
+        <></>
+      )}
+      {track.added_at !== undefined ? (
+        <div className={"TableCell TableCellAddedAt"}>
+          {formatTimeDiff(new Date(track.added_at).getTime(), Date.now())}
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className={"TableCell TableCellDuration"}>
+        {formatTimestamp(track.duration_ms)}
+      </div>
+      {track.liked !== undefined ? (
+        <div className={"TableCell TableCellLiked"}>
+          <Checkbox
+            checked={track.liked}
+            iconCodeChecked={"favorite"}
+            iconCodeUnchecked={"favorite_border"}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
