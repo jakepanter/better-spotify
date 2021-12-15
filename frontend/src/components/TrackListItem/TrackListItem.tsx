19312b89654d7467620d84b8d045716adbd93b06
Malin Dulkies <kay.sian@gmx.de>
2021-12-15 20:18:18 +0100

    added hover effect for songs in table

--- frontend/src/components/TrackListItem/TrackListItem.tsx
+++ frontend/src/components/TrackListItem/TrackListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {AlbumObjectSimplified, ArtistObjectSimplified, TrackObjectFull, TrackObjectSimplified} from "spotify-types";
 import {formatTimeDiff, formatTimestamp} from "../../utils/functions";
+import './TrackListItem.scss';
 import Checkbox from "../Checkbox/Checkbox";
 //import SpotifyWebPlayer from "react-spotify-web-playback";
 
@@ -18,28 +19,30 @@ function TrackListItem(props: Props) {
   const track = props;
 
   return (
-    <div className={'TableRow'}>
-      {track.album !== undefined ?
-        <div className={'TableCell TableCellArtwork'}>
-          <img style={{width: "40px", height: "40px"}} src={track.album.images[2].url} alt=""/>
+      <div className={'TrackListRow'}>
+        <div className={'TableRow'}>
+          {track.album !== undefined ?
+            <div className={'TableCell TableCellArtwork'}>
+              <img style={{width: "40px", height: "40px"}} src={track.album.images[2].url} alt=""/>
+            </div>
+            :
+            <div className={'TableCell TableCellArtwork'} style={{width: "40px", height: "40px"}}/>
+          }
+          <div className={'TableCell TableCellTitleArtist'}>
+            <span className={'TableCellTitle'}>{track.name}</span>
+            <span className={'TableCellArtist'}>{track.artists.map((artist) => artist.name).join(", ")}</span>
+          </div>
+          {track.album !== undefined ?
+            <div className={'TableCell TableCellAlbum'}>{track.album.name}</div>
+            : <></>}
+          {track.added_at !== undefined ? <div className={'TableCell TableCellAddedAt'}>
+            {formatTimeDiff(new Date(track.added_at).getTime(), Date.now())}
+          </div> : <></>}
+          <div className={'TableCell TableCellDuration'}>{formatTimestamp(track.duration_ms)}</div>
+          {track.liked !== undefined ? <div className={'TableCell TableCellLiked'}>
+            <Checkbox checked={track.liked} iconCodeChecked={'favorite'} iconCodeUnchecked={'favorite_border'}/>
+          </div> : <></>}
         </div>
-        :
-        <div className={'TableCell TableCellArtwork'} style={{width: "40px", height: "40px"}}/>
-      }
-      <div className={'TableCell TableCellTitleArtist'}>
-        <span className={'TableCellTitle'}>{track.name}</span>
-        <span className={'TableCellArtist'}>{track.artists.map((artist) => artist.name).join(", ")}</span>
-      </div>
-      {track.album !== undefined ?
-        <div className={'TableCell TableCellAlbum'}>{track.album.name}</div>
-        : <></>}
-      {track.added_at !== undefined ? <div className={'TableCell TableCellAddedAt'}>
-        {formatTimeDiff(new Date(track.added_at).getTime(), Date.now())}
-      </div> : <></>}
-      <div className={'TableCell TableCellDuration'}>{formatTimestamp(track.duration_ms)}</div>
-      {track.liked !== undefined ? <div className={'TableCell TableCellLiked'}>
-        <Checkbox checked={track.liked} iconCodeChecked={'favorite'} iconCodeUnchecked={'favorite_border'}/>
-      </div> : <></>}
     </div>
   );
 }
