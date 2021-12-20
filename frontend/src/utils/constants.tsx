9e1491c70959d40a6769f1d0aad1db1ea74c3cba
Jakob Panten <jakob@panten-sh.de>
2021-12-20 22:52:27 +0100

    add settings for deployment

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-undef
-export const API_URL: string = (process.env.REACT_APP_API_URL as string);
+export const API_URL: string = `${(process.env.REACT_APP_API_URL as string) + process.env.PORT}/`;
