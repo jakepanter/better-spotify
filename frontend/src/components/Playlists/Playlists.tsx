0fc4f3f593e2b66f1cfd1eb08a794a7ab2c1e818
Lennard Grimm <hi@grimmle.com>
2021-12-22 20:04:05 +0100

    wrapper for createNewPlaylist to allow rerendering on state change

--- frontend/src/components/Playlists/Playlists.tsx
+++ frontend/src/components/Playlists/Playlists.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import {
+  CreatePlaylistResponse,
   ListOfUsersPlaylistsResponse,
   PlaylistObjectSimplified,
 } from "spotify-types";
@@ -9,6 +10,7 @@ import { API_URL } from "../../utils/constants";
 import Button from "../Button/Button";
 import "../../cards.scss";
 import CoverPlaceholder from "../CoverPlaceholder/CoverPlaceholder";
+import { createNewPlaylist } from "../../helpers/api-helpers";
 
 interface IProps {}
 
@@ -24,7 +26,7 @@ class Playlists extends Component<IProps, IState> {
       results: [],
     };
 
-    this.createNewPlaylist = this.createNewPlaylist.bind(this);
+    this.handleCreateNewPlaylist = this.handleCreateNewPlaylist.bind(this);
   }
 
   async componentDidMount() {
@@ -36,54 +38,56 @@ class Playlists extends Component<IProps, IState> {
     this.setState((state) => ({ ...state, results: data.items }));
   }
 
-    async createNewPlaylist() {
-        const number = this.state.results ? this.state.results.length + 1 : '-1';
-        const data = {
-            playlistName: "coole neue playlist #" + number,
-            options: {
-                collaborative: false,
-                public: false,
-            },
-        };
-        await fetch(`${API_URL}api/spotify/playlists`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-        });
-        await this.componentDidMount()
-    }
+  async handleCreateNewPlaylist() {
+    const number = this.state.results ? this.state.results.length + 1 : "-1";
+    const options = {
+      collaborative: false,
+      public: false,
+    };
+    const newPlaylist: CreatePlaylistResponse = await createNewPlaylist(
+      "coole neue playlist #" + number,
+      options
+    );
+    const arr = [newPlaylist, ...this.state.results];
+    this.setState((state) => ({
+      ...state,
+      results: arr,
+    }));
+  }
 
-    render() {
-        if (this.state.results.length === 0) return <p>loading...</p>;
-        const playlists = this.state.results.map((playlist) => {
-            return (
-                <li className={"column"} key={playlist.id}>
-                    <Link to={`/playlist/${playlist.id}`}>
-                        {playlist.images.length > 0 ? (
-                            <div
-                                className={"cover"}
-                                style={{ backgroundImage: `url(${playlist.images[0].url})` }}
-                            />
-                        ) : (
-                            <CoverPlaceholder className="cover"/>
-                        )}
-                        <span className={"title"}>{playlist.name}</span>
-                        <span className={"artists-name"}>by {playlist.owner.display_name}</span>
-                    </Link>
-                </li>
-            );
-        });
+  render() {
+    if (this.state.results.length === 0) return <p>loading...</p>;
+    const playlists = this.state.results.map((playlist) => {
+      return (
+        <li className={"column"} key={playlist.id}>
+          <Link to={`/playlist/${playlist.id}`}>
+            {playlist.images.length > 0 ? (
+              <div
+                className={"cover"}
+                style={{ backgroundImage: `url(${playlist.images[0].url})` }}
+              />
+            ) : (
+              <CoverPlaceholder className="cover" />
+            )}
+            <span className={"title"}>{playlist.name}</span>
+            <span className={"artists-name"}>
+              by {playlist.owner.display_name}
+            </span>
+          </Link>
+        </li>
+      );
+    });
 
-        return (
-            <div style={{overflow: 'hidden auto'}}>
-                <h2>My Playlists</h2>
-                <Button onClick={this.createNewPlaylist}>New Playlist</Button>
-                <div className={"overview"}>
-                    <ul className={"overview-items"}>{playlists}</ul>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div style={{ overflow: "hidden auto" }}>
+        <h2>My Playlists</h2>
+        <Button onClick={this.handleCreateNewPlaylist}>New Playlist</Button>
+        <div className={"overview"}>
+          <ul className={"overview-items"}>{playlists}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Playlists;
