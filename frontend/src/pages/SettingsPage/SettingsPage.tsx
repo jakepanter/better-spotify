d9f2dfba921b620773cdd174c276daf5aec8767b
Phillip Kiele <phillipkiele@gmail.com>
2021-12-28 21:19:01 +0100

    Add SettingsPage

---
+++ frontend/src/pages/SettingsPage/SettingsPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SettingsPage.scss";
+
+function SettingsPage() {
+  return <div className={'Settings'}>
+    <div className={'SettingsSection'}>
+      <h2 className={'SettingsSectionHeading'}>Tags</h2>
+      <div className={'SettingsSectionRow'}>
+        {/* TODO */}
+      </div>
+    </div>
+  </div>;
+}
+
+export default SettingsPage;
