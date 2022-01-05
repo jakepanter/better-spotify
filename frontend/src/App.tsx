b7a572dfe6e5a0cd5ba2f8dd5eded5def3bb02fa
Lennard Grimm <hi@grimmle.com>
2022-01-05 13:18:51 +0100

    close contextMenu on any click outside

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -50,6 +50,13 @@ function App() {
       setIsAuthorized(res !== undefined);
     }
     getAccessToken();
+
+    // close contextMenu on any click outside of a icon button
+    document.addEventListener('click', (e: any) => {
+      if (state.contextMenu.isOpen && !e.target.className.includes('material-icon')) {
+        state.setContextMenu({ ...contextMenu, isOpen: false });
+      }
+    })
   }, []);
 
   if (!isAuthorized) {
