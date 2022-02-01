a27d0a88ccac482a5fad4c9eec1a7fdb45c183f7
StefanBoehnert <boehnert.stefan@gmail.com@>
2022-02-01 19:26:32 +0100

    Fixed Headers and added 'Added at' column

--- frontend/src/components/TrackList/TrackList.tsx
+++ frontend/src/components/TrackList/TrackList.tsx
@@ -349,6 +349,7 @@ function TrackList(props: Props) {
                 <div className={"TableCell TableCellAlbum"}>Album</div>
                 <div className={"TableCell TableCellDuration"}>Duration</div>
                 <div className={"TableCell TableCellTags"}>Tags</div>
+                <div className={"TableCell TableCellActions"}>Playlist</div>
               </div>
               <div className={"TableBody"}>
                 {props.tracks.map((item, index) => {
