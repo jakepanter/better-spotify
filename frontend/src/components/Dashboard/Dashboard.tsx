b9ac8556feeb2703da253e132f9e76ace037467b
StefanBoehnert <boehnert.stefan@gmail.com@>
2022-02-05 19:18:32 +0100

    Removed favorites from standard grid. Switched to top 50 DE

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -138,37 +138,38 @@ export class DashboardService {
 }
 
 const DEFAULT_DASHBOARD_STATE: IState = {
-  layouts:{
-    md: [
-      { w: 1, h: 2, x: 1, y: 0, i: "favorites", moved: false, static: false },
-      { w: 1, h: 2, x: 0, y: 0, i: "albums", moved: false, static: false },
-      { w: 1, h: 2, x: 0, y: 2, i: "playlists", moved: false, static: false },
-      { w: 1, h: 2, x: 1, y: 2, i: "top-daily-global", moved: false, static: false },
+  "layouts":
+    {"md":[
+      {"w":1,"h":2,"x":0,"y":0,"i":"albums","moved":false,"static":false},
+      {"w":1,"h":2,"x":0,"y":2,"i":"playlists","moved":false,"static":false},
+      {"w":1,"h":2,"x":1,"y":0,"i":"top-daily-global","moved":false,"static":false},
+      {"w":1,"h":2,"x":1,"y":2,"i":"top-daily-DE","moved":false,"static":false}
     ],
-    xl: [
-      { w: 2, h: 2, x: 2, y: 0, i: "favorites", moved: false, static: false },
-      { w: 2, h: 2, x: 0, y: 2, i: "albums", moved: false, static: false },
-      { w: 2, h: 2, x: 0, y: 0, i: "playlists", moved: false, static: false },
-      { w: 2, h: 2, x: 2, y: 2, i: "top-daily-global", moved: false, static: false },
+    "xl":[
+      {"w":2,"h":2,"x":2,"y":0,"i":"top-daily-de","moved":false,"static":false},
+      {"w":2,"h":2,"x":0,"y":2,"i":"albums","moved":false,"static":false},
+      {"w":2,"h":2,"x":0,"y":0,"i":"playlists","moved":false,"static":false},
+      {"w":2,"h":2,"x":2,"y":2,"i":"top-daily-global","moved":false,"static":false}
     ],
-    sm: [
-      { w: 1, h: 2, x: 0, y: 0, i: "favorites", moved: false, static: false },
-      { w: 1, h: 2, x: 0, y: 2, i: "albums", moved: false, static: false },
-      { w: 1, h: 2, x: 0, y: 4, i: "playlists", moved: false, static: false },
-      { w: 1, h: 2, x: 0, y: 6, i: "top-daily-global", moved: false, static: false },
+    "sm":[
+      {"w":1,"h":2,"x":0,"y":0,"i":"albums","moved":false,"static":false},
+      {"w":1,"h":2,"x":0,"y":2,"i":"playlists","moved":false,"static":false},
+      {"w":1,"h":2,"x":0,"y":4,"i":"top-daily-global","moved":false,"static":false},
+      {"w":1,"h":1,"x":0,"y":6,"i":"top-daily-DE","moved":false,"static":false}
     ]},
-  albums: [],
-  playlists: [],
-  charts: [
-    { countryCode: "global", chartType: "top", period: "daily" }
+  "albums":[],
+  "playlists":[],
+  "charts":[
+    {"countryCode":"global","chartType":"top","period":"daily"},
+    {"countryCode":"DE","chartType":"top","period":"daily"}
   ],
-  tagTracklists: [],
-  showFavorites: true,
-  showAlbums: true,
-  showPlaylists: true,
-  chartSelection: { countryCode: "global", type: "top", period: "daily" },
-  tagTracklistsSelection: "",
-  width: 1475
+  "tagTracklists":[],
+  "showFavorites":false,
+  "showAlbums":true,
+  "showPlaylists":true,
+  "chartSelection":{"countryCode":"DE","type":"top","period":"daily"},
+  "tagTracklistsSelection":"",
+  "width":195
 };
 
 class Dashboard extends Component<IProps, IState> {
